3efbe3284922061aabcb928acb84eedc85b3f721
inchangson <3721cha21.11@gmail.com>
2022-04-19 16:50:57 +0900

    update #2 : setup django-environ in settings.py

--- withme/settings.py
+++ withme/settings.py
@@ -9,21 +9,35 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+import environ
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# environ setting - security for AWS DB
+# https://django-environ.readthedocs.io/en/latest/
+# get env
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, True)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gwqc#k^*s$2(emxz8@aj1kds)@96epb3b#=hv^9-d*rp$5+ok_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -80,15 +94,23 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': '1111',
-        'HOST': '39.118.206.39',
-        'PORT': '4544',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'test',
+    #     'USER': 'root',
+    #     'PASSWORD': '1111',
+    #     'HOST': '39.118.206.39',
+    #     'PORT': '4544',
+    # }
+    'default' :
+    {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
-
 }
 
 ##################
