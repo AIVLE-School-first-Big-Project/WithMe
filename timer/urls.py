6cd659ab1969bf57b64705f3d7b61ca416e8a588
inchangson <3721cha21.11@gmail.com>
2022-05-03 14:42:29 +0900

    Feat #18 : set start time

--- timer/urls.py
+++ timer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("watch/", views.watch, name='watch'),
     path("timer/", views.timer, name='timer'),
     path("stopwatch/", views.stopwatch, name='stopwatch'),
+    path("set_start_time/", views.set_start_time, name='set_start_time'),
 ]
--- timer/views.py
+++ timer/views.py
@@ -3,13 +3,15 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from django.shortcuts import redirect
 from timer.forms import *
-from django.http import HttpResponseRedirect
+from tag.models import *
+from django.http import HttpResponseRedirect, JsonResponse
+import json
+from django.utils import timezone
 
 @login_required
 def watch(request):
     form = timeForm(request.POST)
     if request.method == 'POST':
-        
         if form.is_valid(): # 데이터 저장/ 어딘가로 이동/메세지 출력 등등
             timer_data = form.save(commit = False)
             timer_data.tag_name = request.POST.get('Tag')
@@ -21,10 +23,7 @@ def watch(request):
         else:
             form = timeForm()
     return render(request, 'timer/watch.html', {'form':form})
-
-
-
-
+    
 @login_required
 def timer(request):
 
@@ -35,3 +34,21 @@ def timer(request):
 def stopwatch(request):
 
     return render(request, 'timer/stopwatch.html')
+
+@login_required
+def set_start_time(request):
+    obj = request.body.decode("utf-8")
+    data = json.loads(obj)
+
+    start_time = data['time']
+    tag = data['tag']
+    print('\n'*5, f'{type(start_time)}|| start_time : {start_time}', '\n'*5)
+    print('\n'*5, f'{type(tag)}|| tag : {tag}', '\n'*5)
+
+    # user_log = UserLog()
+    # user_log.user = request.user
+    # user_log.tag = Tag.objects.get(id=tag_id)
+    # user_log.start_time = timezone.now()
+    # user_log.save()
+
+    return JsonResponse(data)
