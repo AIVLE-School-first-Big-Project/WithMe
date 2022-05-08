fa8ed914ba1f75ad485d1d206201265cc451eadf
inchangson <3721cha21.11@gmail.com>
2022-05-08 19:28:16 +0900

    Feat #26 : Post(CRUD), bulletin main page

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -4,9 +4,13 @@ from . import views
 app_name = 'bulletin'
 urlpatterns = [
     # 기본 페이지
-    path('', views.board, name='board'),
-    # 해당 feed_id 게시글
-    path('<int:feed_id>/', views.feed),
-    # 글쓰기
-    path('upload/', views.upload, name='upload'),
+    path('', views.bulletin, name='bulletin'),
+    
+    # 게시글 조회, 추가, 수정, 삭제
+    path('<int:post_id>/', views.post),
+    path('upload/', views.upload_post, name='upload_post'),
+    path('<int:post_id>/modify/', views.modify_post, name='modify_post'),
+    path('<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    
+
 ]
