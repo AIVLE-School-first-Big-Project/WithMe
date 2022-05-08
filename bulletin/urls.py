9451daf86d55a3c56777f773b726822597fd1a86
inchangson <3721cha21.11@gmail.com>
2022-05-08 20:32:11 +0900

    Feat #26 : upload Post

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     
     # 게시글 조회, 추가, 수정, 삭제
     path('<int:post_id>/', views.post),
-    path('upload/', views.upload_post, name='upload_post'),
+    path('upload_post/', views.upload_post, name='upload_post'),
     path('<int:post_id>/modify/', views.modify_post, name='modify_post'),
     path('<int:post_id>/delete/', views.delete_post, name='delete_post'),
     
